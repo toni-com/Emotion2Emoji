01d059151f5f44c7b71789eb2bfa5457c0fe80be
toni <nickschtino@gmail.com>
2025-11-24 18:33:17 +0100

    fix

--- src/dataset.py
+++ src/dataset.py
@@ -23,7 +23,7 @@ def get_data_loaders(
     test_transform = transforms.Compose(
         [
             transforms.Grayscale(num_output_channels=3),
-            transforms.Resize(256),
+            transforms.Resize((224, 224)),
             transforms.ToTensor(),
             transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
         ]
